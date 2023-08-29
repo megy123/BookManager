9b78dc0e6dbd83074866364aa49caf3b081039cb
megy <dominik000@pobox.sk>
2023-08-29 21:59:13 +0200

    Frontend code for mainform.

--- BookManager/Forms/MainForm.vb
+++ BookManager/Forms/MainForm.vb
@@ -15,6 +15,8 @@
         'Label6.Text = "Read pages: " & user.GetPageCount()
 
         user = New User(False)
+        selectedBook = Nothing
+
         'MessageBox.Show(user.lib_path)
         'Dim bok As New Book("C:\Users\domin\Desktop\test.pdf")
         'Dim bok As New Book("C:\Users\domin\Desktop\Ask for More 10 Questions to Negotiate Anything - Alexandra Carter.pdf")
@@ -24,9 +26,25 @@
 
     Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
         'Read button
+        If selectedBook IsNot Nothing Then
+            MessageBox.Show(user.lib_path & "\" & selectedBook.path & "\" & selectedBook.title & " - " & selectedBook.autor & ".pdf")
+            Process.Start(user.lib_path & "\" & selectedBook.path & "\" & selectedBook.title & " - " & selectedBook.autor & ".pdf")
+        End If
     End Sub
 
     Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
         'Book info button
+        If selectedBook IsNot Nothing Then
+
+        End If
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        'Settings button
+        Settings.Show()
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        'Search button
     End Sub
 End Class
