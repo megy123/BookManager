1cb01cdccafe63784308160822194a24e3ce5504
megy <dominik000@pobox.sk>
2024-07-15 14:20:36 +0200

    added: Id remover

---
+++ IDRemover/Form1.Designer.vb
@@ -0,0 +1,180 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.FolderBrowserDialog1 = New System.Windows.Forms.FolderBrowserDialog()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.GroupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'OpenFileDialog1
+        '
+        Me.OpenFileDialog1.FileName = "OpenFileDialog1"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(12, 12)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(115, 30)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "open file"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 65)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(39, 13)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Label1"
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(145, 12)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(115, 30)
+        Me.Button2.TabIndex = 2
+        Me.Button2.Text = "get custom props"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(224, 154)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(115, 30)
+        Me.Button3.TabIndex = 3
+        Me.Button3.Text = "set prop"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(12, 164)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox1.TabIndex = 4
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(118, 164)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox2.TabIndex = 5
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.ComboBox1)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Controls.Add(Me.Button5)
+        Me.GroupBox1.Controls.Add(Me.Button4)
+        Me.GroupBox1.Location = New System.Drawing.Point(355, 12)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(217, 172)
+        Me.GroupBox1.TabIndex = 6
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Id remover"
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(136, 20)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(75, 23)
+        Me.Button4.TabIndex = 0
+        Me.Button4.Text = "Remove IDs"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Button5
+        '
+        Me.Button5.Location = New System.Drawing.Point(7, 20)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(84, 23)
+        Me.Button5.TabIndex = 1
+        Me.Button5.Text = "select folder"
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(85, 105)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(52, 31)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "0/0"
+        Me.Label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Items.AddRange(New Object() {"Version 1.0"})
+        Me.ComboBox1.Location = New System.Drawing.Point(7, 53)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(204, 21)
+        Me.ComboBox1.TabIndex = 3
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(584, 196)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents OpenFileDialog1 As OpenFileDialog
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents Button5 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents FolderBrowserDialog1 As FolderBrowserDialog
+    Friend WithEvents Label2 As Label
+    Friend WithEvents ComboBox1 As ComboBox
+End Class
---
+++ IDRemover/Form1.vb
@@ -0,0 +1,90 @@
+﻿Imports FxResources
+Imports PdfSharp.Pdf
+Imports PdfSharp.Pdf.IO
+Imports System.IO
+Public Class Form1
+    Dim doc As PdfDocument
+    Dim path As String
+    Dim folder_path As String
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If OpenFileDialog1.ShowDialog() = DialogResult.OK Then
+            doc = PdfReader.Open(OpenFileDialog1.FileName)
+            path = OpenFileDialog1.FileName
+        End If
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Dim props As PdfDictionary.DictionaryElements = doc.Info.Elements
+
+        Label1.Text = ""
+        For Each kvp As KeyValuePair(Of String, PdfItem) In props
+            Label1.Text &= kvp.Key & " : " & kvp.Value.ToString() & vbNewLine
+        Next
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        doc.Info.Elements.Add(New KeyValuePair(Of String, PdfItem)(TextBox1.Text, New PdfString(TextBox2.Text)))
+        doc.Save(path)
+    End Sub
+
+#Region "ID-remover"
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        If FolderBrowserDialog1.ShowDialog() = DialogResult.OK Then
+            folder_path = FolderBrowserDialog1.SelectedPath
+        End If
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        If Not folder_path = "" Then
+            If ComboBox1.SelectedIndex = 0 Then
+                removeV1Ids(folder_path)
+            Else
+                MessageBox.Show("No selected version.")
+            End If
+        Else
+            MessageBox.Show("No path selected")
+        End If
+    End Sub
+
+    Private Sub removeV1Ids(path As String)
+        Dim local_books As List(Of String)
+
+        local_books = getAllFiles(path)
+        Dim bookCount As Integer = local_books.Count
+        Dim processedBooks As Integer = 0
+        Label2.Text = "0/" & bookCount
+
+        For Each s As String In local_books
+            'Read header from file
+            Dim bytes As Byte() = File.ReadAllBytes(s)
+            'Check if header valid v1 ID
+            If bytes(0) = &H25 And bytes(1) = &HBB And bytes(2) = &H12 And bytes(7) = &H10 Then
+                Dim newBytes(bytes.Length - 9) As Byte
+                Buffer.BlockCopy(bytes, 8, newBytes, 0, bytes.Length - 8)
+                File.WriteAllBytes(s, newBytes)
+            End If
+
+            processedBooks += 1
+            Label2.Text = processedBooks & "/" & bookCount
+        Next
+    End Sub
+
+    Private Function getAllFiles(path As String) As List(Of String)
+        Dim files As New List(Of String)
+        If System.IO.Directory.Exists(path) Then
+            For Each f As String In System.IO.Directory.GetFiles(path)
+                If f.EndsWith(".pdf") Then
+                    files.Add(f)
+                End If
+            Next
+            For Each d As String In System.IO.Directory.GetDirectories(path)
+                files.AddRange(getAllFiles(d))
+            Next
+        Else
+            MessageBox.Show("Non valid library path:""" & path & """.")
+        End If
+        Return files
+    End Function
+
+#End Region
+End Class
